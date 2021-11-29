a42f80ea5b1a667018335b91ebc4918d760abfe9
StuporNova33 <63005660+stupornova33@users.noreply.github.com>
2021-11-28 22:09:47 -0600

    edited loop logic

--- parse_results.R
+++ parse_results.R
@@ -8,16 +8,17 @@ require(jsonlite)
 # remember to replace Inf values with "Inf" using
 # sed -i 's/inf/"inf"/'
 # run using <Rscript parse_results.R>
-setwd("C:/Users/tjarva/parse_absREL_json/")
+#setwd("C:/Users/path/to/json/files")
 
 
 files <- list.files(full.names = TRUE, pattern = '*.json$')
-string <- "pal.ABSREL.json"
+print(files)
 
+string <- "p2n.ABSREL.json"
 
 for(file in files){
-  global_results <- data.frame(name = character(0), branch = character(0), omega1 = character(0), percent1 = character(0), omega2 = character(0), 
-                             percent2 = character(0), omega3 = character(0), percent3 = character(0), stringsAsFactors = FALSE)
+  global_results <- data.frame(name = character(0), branch = character(0), omega1 = character(0), percent1 = character(0), omega2 = character(0),
+                               percent2 = character(0), omega3 = character(0), percent3 = character(0), stringsAsFactors = FALSE)
   name <- strsplit(paste(file), split = string)
   simplejson <- fromJSON(file)
   index <- 1
@@ -27,34 +28,36 @@ for(file in files){
     branch <- names(branchnames[index])
     local_results <- data.frame(name = character(0), branch = character(0), omega1 = character(0), percent1 = character(0), omega2 = character(0),
                                 percent2 = character(0), omega3 = character(0), percent3 = character(0), stringsAsFactors = FALSE)
-  if(i$`Corrected P-value` > 0.05){
-    next
-  } else if(i$`Corrected P-value` <= 0.05){
+    if(i$`Corrected P-value` > 0.05){
+      index <- index + 1
+      next
+    } else if(i$`Corrected P-value` <= 0.05){
       local_results[1, 1] <- name
       local_results[1, 2] <- branch
-        if(i$`Rate classes` == 2){
-          local_results[1, 3] <- i$`Rate Distributions`[1,1]
-          local_results[1, 4] <- i$`Rate Distributions`[1,2]
-          local_results[1, 5] <- i$`Rate Distributions`[2,1]
-          local_results[1, 6] <- i$`Rate Distributions`[2,2]
-          local_results[1, 7] <- "NA"
-          local_results[1, 8] <- "NA"
-        } else if(i$`Rate classes` == 3){
-           local_results[1, 3] <- i$`Rate Distributions`[1,1]
-           local_results[1, 4] <- i$`Rate Distributions`[1,2]
-           local_results[1, 5] <- i$`Rate Distributions`[2,1]
-           local_results[1, 6] <- i$`Rate Distributions`[2,2]
-           local_results[1, 7] <- i$`Rate Distributions`[3,1]
-           local_results[1, 8] <- i$`Rate Distributions`[3,2]
-        }
+      if (i$`Rate classes` == 2){
+        local_results[1, 3] <- i$`Rate Distributions`[1,1]
+        local_results[1, 4] <- i$`Rate Distributions`[1,2]
+        local_results[1, 5] <- i$`Rate Distributions`[2,1]
+        local_results[1, 6] <- i$`Rate Distributions`[2,2]
+        local_results[1, 7] <- "NA"
+        local_results[1, 8] <- "NA"
+      } else if(i$`Rate classes` == 3){
+        local_results[1, 3] <- i$`Rate Distributions`[1,1]
+        local_results[1, 4] <- i$`Rate Distributions`[1,2]
+        local_results[1, 5] <- i$`Rate Distributions`[2,1]
+        local_results[1, 6] <- i$`Rate Distributions`[2,2]
+        local_results[1, 7] <- i$`Rate Distributions`[3,1]
+        local_results[1, 8] <- i$`Rate Distributions`[3,2]
+      }
     }
-    
     global_results <- dplyr::bind_rows(global_results, local_results)
     rm(local_results)
     index <- index + 1
     
   }
   global_results <- na.omit(global_results)
-  write.table(global_results, file = "parsed_absrel_json.txt", sep = "\t", row.names = FALSE, col.names = TRUE, append = TRUE, quote = FALSE)
+  
+  if (nrow(global_results) > 0) {
+    write.table(global_results, file = "parsed_absrel_json.txt", sep = "\t", row.names = FALSE, col.names = FALSE, append = TRUE, quote = FALSE)
+  }
 }
-
